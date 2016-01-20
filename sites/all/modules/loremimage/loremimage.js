796eebb95b6faacc3e12302aab46f4baad56351a
Tamara Reed <jeffntamara@yahoo.com>
2016-01-20 12:10:57 -0800

    Completed validation and fixed it to show all items in one field.  Also, formed URL.

--- sites/all/modules/loremimage/loremimage.js
+++ sites/all/modules/loremimage/loremimage.js
@@ -1,27 +1,53 @@
 
 
 function sendToLoremImage() {
+    var responses = "";
     var lorempixel = "http://lorempixel.com/";
     var xwidth = document.forms["loremimage-form"]["xwidth"].value;
     var yheight = document.forms["loremimage-form"]["yheight"].value;
+    var category = document.forms["loremimage-form"]["category"].value; 
+    var incolor = document.forms["loremimage-form"]["color"].value; 
+    var dummytext = document.forms["loremimage-form"]["dummytext"].value;
+    var color = "";
+    var categories = ["None", "Abstract", "City", "People", "Transport", "Animals", "Food", "Nature", "Business", "Cats", "Fashion", "Technics"];
     if (xwidth == null || xwidth == "") {
-        alert("X dimensions must be entered");
-        return false;
+        var responses = responses + "X dimensions must be entered\n";
     }
     else if (isNaN(xwidth) == true) {
-	    alert("X dimensions must a number");
-	    return false;
+	    var responses = responses + "X dimensions must a number\n";
     }
-    else if (yheight == null || yheight == "") {
-        alert("Y dimensions must be entered");
-        return false;
+    if (yheight == null || yheight == "") {
+        var responses = responses + "Y dimensions must be entered\n";
     }
     else if (isNaN(yheight) == true) {
-	    alert("Y dimensions must a number");
-	    return false;
+	    var responses = responses + "Y dimensions must a number\n";
+    }
+    if (category == "0") {
+	    var responses = responses + "Please select a category\n";
     }
     else {
-	    console.log(lorempixel + xwidth + "/" + yheight );
+	    category = categories[category];
+    }
+    if ((incolor != '0') && (incolor != '1')){
+        var responses = responses + "Please choose color or greyscale\n";
+    }
+    else if (incolor == '0') {
+        color = "color";
+    }
+    else if (incolor == '1') {
+        color = "greyscale";
+    }
+    if (dummytext == null || dummytext == "") {
+        var responses = responses + "Dummy Text must be entered";
+    }
+    if (responses == null || responses == "") {
+	    console.log(lorempixel + xwidth + "/" + yheight + "/" + category + "/" + dummytext + "/" + color);
+	    return false;
+    }
+    else
+    {
+	    console.log(category);
+	    alert(responses);
 	    return false;
     }
 }
